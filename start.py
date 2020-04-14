ac70abc87453221f0a3e74f21ffffa527960d4ab
tahiti444 <davidgalvemasip@gmail.com>
2020-04-14 22:18:32 +0200

    added class to construct API's url from foursquare

--- start.py
+++ start.py
@@ -10,40 +10,62 @@ Retrieving data from foursquare's API:
         + Version (usufull for controling your app)
 
 '''
+
 from pathlib import Path
 import json
 
+# Get credentials from json file
+#########################################################################
 wksPath = Path(__file__).parents[0]
-
+# Open json file and return content of it
 def readFile(nameFile):
     path = wksPath /  nameFile
     with open(path, 'r') as file:
         content = file.read()
         return content
 
-# creating an object of URL for foursquare's API
-main_URL = 'https://api.foursquare.com/v2/'
 
-# TODO:generalize this for future...
-# What do you want to search?
-about = 'venues/search?' # venues, users or tips
-location = '40.7,-74'
-what = 'coffee'
-query = 'll='+str(location)+'&query='+what
-
-# inizializate id credentials
+# Globals initialization
+#########################################################################
+# read json file and save it
 id_key = readFile('credentials.json')
 id_key = json.loads(id_key)
+# inizializate id credentials
 client_id = id_key["client_id"]
 client_secret = id_key["client_secret"]
-
 version = str(20180602)
-credentials = '&client_id='+client_id+'&client_secret='+client_secret+'&v='+version
+# object for URL from foursquare's API
+main_URL = 'https://api.foursquare.com/v2/'
 
-# forming complete url
-foursquare_URL = main_URL + about + query + credentials 
 
-print (foursquare_URL)
+# TODO:missing url_from_users and url_from_tips...
+# Classes
+##########################################################################
+'''
+class Link() -> generates a link to query the json file out of foursquare's API:
+    method url_from_venue -> generates this link for venues and needs of: 
+        option,         venues
+        location,       as: latitud, longitud
+        query           as: coffee, chinese food, windsurf...
+'''
+class Link(object):
+
+    global main_URL, client_id, client_secret, version
+
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+
+    def url_from_venue(self):
+        self.option = str(self.option) + '/search?'
+        self.location = 'll=' + self.location
+        self.query = '&query=' + self.query
+        return (str(main_URL+self.option+self.location+self.query+'&client_id='+client_id+'&client_secret='+client_secret+'&v='+version))
+
+
+
+coffeeNY = Link(option='venues', location='40.7,-74', query='coffee').url_from_venue()
+print(coffeeNY)
+
 
 
 
